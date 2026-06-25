680abcd48d69bcc8c47445e93fdc16d416db443c
Atharva Shinde <atharva.solves@gmail.com>
2026-06-25 23:08:08 +0530

    repository and repositories implementation ,read and write

---
+++ lib/features/note_app/data/repositories_implementation/note_repositories_impl.dart
@@ -0,0 +1,48 @@
+//actuall implementation
+//repo <--repoImpl-->Local Storage DATA SOURCE
+
+//why?
+//1.  Single Responsibility
+//    (why not ctr-->Data source? ctrl only UI update.not second responsibitlity o translating data or multiple Data source manage)
+
+//2. future proof swap game:if firebase integration.
+//   without repo, direct LDS conn ,each code change from LDS to FB in UI
+//   with repo ,Ctr only talk to boss(reop).
+
+import 'package:note_app/features/note_app/data/data_sources/note_local_data_source.dart';
+import 'package:note_app/features/note_app/data/models/note_model.dart';
+import 'package:note_app/features/note_app/domain/repositeries/note_repositories.dart';
+
+class NoteRepositoriesImpl implements NoteRepositories {
+  //fin _fields,construct,@ov meth
+  final NoteLocalDataSource _localDataSource;
+
+  NoteRepositoriesImpl({required NoteLocalDataSource localDataSource})
+    : _localDataSource = localDataSource;
+
+  //read
+  @override
+  List<NoteModel> getNotes() {
+    // TODO: implement getNotes
+
+    //raw data fetch - map -store in var- return
+
+    List<dynamic> rawData = _localDataSource.getNotesFromStorage();
+    List<NoteModel> listOfNoteModel = rawData
+        .map((rawMap) => NoteModel.fromJson(rawMap))
+        .toList();
+
+    return listOfNoteModel;
+  }
+
+  //write
+  @override
+  Future<void> saveNotes(List<NoteModel> notes) async {
+    //noteM--toJson--send.
+    List<Map<String, dynamic>> rawData = notes
+        .map((noteModel) => noteModel.toJson())
+        .toList();
+
+    await _localDataSource.saveNotesToStorage(rawData);
+  }
+}
---
+++ lib/features/note_app/domain/repositeries/note_repositories.dart
@@ -0,0 +1,11 @@
+//contract of rules,
+//(ctr,UI : models)<--Repo-->(local storage:maps)
+import 'package:note_app/features/note_app/data/models/note_model.dart';
+
+abstract class NoteRepositories {
+  //rule 1 must fetch list in NoteModel
+  List<NoteModel> getNotes();
+
+  //rule 2 must save list of models in storage
+  Future<void> saveNotes(List<NoteModel> notes);
+}
