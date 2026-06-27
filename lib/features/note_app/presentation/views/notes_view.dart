9360991df35c44ae872b8e5513ae485200e1d939
Atharva Shinde <atharva.solves@gmail.com>
2026-06-27 13:23:13 +0530

    add NoteView (dummy),improved mistake of commiting with error (in previous commit)

---
+++ lib/features/note_app/presentation/views/notes_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class NotesView extends StatelessWidget {
+  const NotesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Note View Coming Soon...')),
+    );
+  }
+}
