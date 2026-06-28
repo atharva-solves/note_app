4f74da0f01cd50b04510f8bfd8a5280fd3a2a0fc
Atharva Shinde <atharva.solves@gmail.com>
2026-06-28 14:01:41 +0530

    add Dummy AddEditNoteView

---
+++ lib/features/note_app/presentation/views/unified_add_edit_note_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class UnifiedAddEditNoteView extends StatelessWidget {
+  const UnifiedAddEditNoteView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Add/Edit View coming soon...'),
+      ),
+    );
+  }
+}
