c6d47310b8dcb3d8434d736cf2e4db6e32feab3d
Atharva Shinde <atharva.solves@gmail.com>
2026-07-21 12:18:43 +0530

    add:login route name in App Routes

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,8 +1,10 @@
 abstract class AppRoutes {
-  //1 priv constructor to avoid instantiation whi is redundant
+  //1 priv constructor to avoid instantiation which is redundant
   AppRoutes._();
 
 //var
+  
+  static const login='/login';
   static const home = '/notes';
   static const editNote = '/edit-note';
 }
